3a9c9fb9ad171feebea22221ea106511955031ce
obama <864589477@qq.com>
2020-07-28 16:05:12 +0800

    correct MaskMobilePix2Pix.py netG_loss

--- models/MaskMobilePix2Pix.py
+++ models/MaskMobilePix2Pix.py
@@ -278,7 +278,7 @@ class MaskMobilePix2PixModel(nn.Module):
         # Fake; stop backprop to the generator by detaching fake_B
         fake_AB = torch.cat((self.real_A, self.fake_B), 1)  # we use conditional GANs; we need to feed both input and output to the discriminator
         pred_fake = self.netD(fake_AB.detach())
-        self.loss_D_fake = self.criterionGAN(pred_fake, False, for_discriminator=False)
+        self.loss_D_fake = self.criterionGAN(pred_fake, False)
         # Real
         real_AB = torch.cat((self.real_A, self.real_B), 1)
         pred_real = self.netD(real_AB)
@@ -292,7 +292,7 @@ class MaskMobilePix2PixModel(nn.Module):
         # First, G(A) should fake the discriminator
         fake_AB = torch.cat((self.real_A, self.fake_B), 1)
         pred_fake = self.netD(fake_AB)
-        self.loss_G_GAN = self.criterionGAN(pred_fake, True)
+        self.loss_G_GAN = self.criterionGAN(pred_fake, True, for_discriminator=False)
         # Second, G(A) = B
         self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
         # mask weight decay
